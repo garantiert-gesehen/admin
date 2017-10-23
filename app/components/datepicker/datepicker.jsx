f9dbba5a965513babfa9b167f9aad6e8907075df
Alexey Gerasimenko <age@marketlogicsoftware.com>
2017-10-23 03:37:44 +0200

    Date Picker

---
+++ app/components/datepicker/datepicker.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import UIDatePicker from 'material-ui/DatePicker';
+
+import './datepicker.scss';
+
+const Datepicker = UIDatePicker;
+
+export default Datepicker;
---
+++ app/components/widget/_date/widget_date.jsx
@@ -0,0 +1,67 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import moment from 'moment';
+import FontIcon from 'react-toolbox/lib/font_icon';
+
+import Placeholder from '../__placeholder/widget__placeholder.jsx';
+import Datepicker from '../../datepicker/datepicker.jsx';
+
+import './widget_date.scss';
+
+const FORMAT = 'DD.MM.YYYY';
+
+class WidgetDate extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      focused: false
+    }
+  }
+
+  componentDidUpdate(prevProps) {
+    if (!prevProps.active && this.props.active && this.datepicker) {
+      console.log(ReactDOM.findDOMNode(this.datepicker).querySelector('input'));
+      ReactDOM.findDOMNode(this.datepicker).querySelector('input').click();
+    }
+  }
+  onChange = (e, date) => {
+    const value = date && date.toString() || null;
+    if (this.props.value !== value) {
+      this.props.update(value);
+    }
+  };
+
+  clear = (e) => {
+    e.stopPropagation();
+    e.preventDefault();
+    this.props.update(null);
+  };
+
+  render() {
+    const { title, active, value } = this.props;
+    const date = value ? new Date(value) : null;
+    const displayDate = date ? moment(date).format(FORMAT) : null;
+
+    return (
+      <div className="widget widget_date" ref={(element) => { this.datepicker = element; }}>
+        <Datepicker
+          mode="landscape"
+          autoOk
+          textFieldStyle={{ display: 'none' }}
+          hintText={`Set ${title}`}
+          container="inline"
+          onChange={this.onChange}
+          value={date}
+        />
+        {!active && <Placeholder hint={`Set ${title}`} value={displayDate} />}
+        {!!value && <FontIcon className="widget__clear" value="close" onClick={this.clear} />}
+      </div>
+    );
+  }
+}
+
+WidgetDate.defaultProps = {
+};
+
+export default WidgetDate;
